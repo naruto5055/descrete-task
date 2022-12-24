70b409b5a68d9085a707b3f94e6d173b8fd734d5
naruto5055 <111577850+naruto5055@users.noreply.github.com>
2022-12-24 22:38:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, I, sum, start, end;
+            Console.WriteLine("starting Number");
+            start
+               = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ending Number");
+            end = int.Parse(Console.ReadLine());
+            for (num =start; num <= end; num++)
+            {
+                sum = 0;
+                for (I = 2; I <= num / 2; I++)
+                {
+                    if (num % I == 0)
+                    {
+                        sum++;
+                        break;
+                    }
+                }
+                if (sum == 0 && num!= 1 && num!= 0)
+                    Console.WriteLine("{0}", num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
